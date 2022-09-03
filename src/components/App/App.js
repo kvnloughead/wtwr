64b1c5601d5ebe4582361d4ea53dc293a18e8673
Kevin Loughead <kvnloughead@gmail.com>
2022-09-03 13:56:23 -0400

    Add Header component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,17 @@
 import Header from '../Header/Header';
 import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
 import './App.css';
 
 function App() {
   return (
-    <>
-      <Header></Header>
-      <Main></Main>
-    </>
+    <div className='page'>
+      <div className='page__wrapper'>
+        <Header></Header>
+        <Main></Main>
+        <Footer></Footer>
+      </div>
+    </div>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import logo from '../../images/logo.svg';
+import avatar from '../../images/placeholder.png';
 import './Header.css';
 
 const Header = () => {
-  return <></>;
+  const date = new Date().toLocaleString('default', {
+    month: 'long',
+    day: 'numeric',
+  });
+
+  const city = 'Pottstown';
+
+  return (
+    <header className='header'>
+      <img className='header__logo' src={logo} alt='WTWR logo' />
+      <p className='header__time-and-place'>
+        <time className='header__time' dateTime={date}>
+          {date}
+        </time>
+        , {city}
+      </p>
+      <button className='header__add-clothes-button'>+ Add Clothes</button>
+      <p className='header__username'>Kevin Loughead</p>
+      <img className='header__avatar' src={avatar} alt="User's avatar" />
+    </header>
+  );
 };
 
 export default Header;
