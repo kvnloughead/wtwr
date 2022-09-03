b4346f57ade7c4c1700a3a82edcd97d852dd8b5c
Kevin Loughead <kvnloughead@gmail.com>
2022-09-03 11:05:13 -0400

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import Header from '../Header/Header';
+import Main from '../Main/Main';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+    </>
+  );
+}
+
+export default App;
