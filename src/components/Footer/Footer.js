64cf5932812ebb3d94462e18bfe26bdff47b2e7b
Kevin Loughead <kvnloughead@gmail.com>
2022-09-03 17:25:41 -0400

    Add Footer markup and styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-  return <></>;
+  return (
+    <footer className='footer'>
+      <span class='footer__text'>
+        Developed by Practicum Grad Kevin Loughead
+      </span>
+      <span className='footer__year'>2022</span>
+    </footer>
+  );
 };
 
 export default Footer;
