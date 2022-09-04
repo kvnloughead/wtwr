d14458eda45e2330759291b4daaa65cdf3d0e5d6
Kevin Loughead <kvnloughead@gmail.com>
2022-09-04 17:08:09 -0400

    Add like buttons and minimal handler

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -4,13 +4,26 @@ import PropTypes, { func, number, string } from 'prop-types';
 import './ItemCard.css';
 
 function ItemCard({ data, openItemModal }) {
+  const [liked, setLiked] = React.useState(false);
+  const openModal = (evt) => {
+    if (!evt.target.classList.contains('card__like-button'))
+      openItemModal({ ...data, visible: true });
+  };
+
   return (
-    <li
-      role="presentation"
-      className="card"
-      onClick={() => openItemModal({ ...data, visible: true })}
-    >
-      <h3 className="card__name">{data.name}</h3>
+    <li role="presentation" className="card" onClick={openModal}>
+      <div className="card__header">
+        <h3 className="card__name">{data.name}</h3>
+        <button
+          onClick={() => setLiked(!liked)}
+          className={`card__like-button ${
+            liked ? 'card__like-button_liked' : ''
+          }`}
+          aria-label="like button"
+          type="button"
+        />
+      </div>
+
       <img
         className="card__image"
         src={data.link}
