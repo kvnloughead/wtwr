09d8ff1f98b81ab3bca2efe27926224a8039dc23
Kevin Loughead <kvnloughead@gmail.com>
2022-11-04 16:12:34 -0400

    Make weather enum case insensitive

--- backend/models/clothingItem.js
+++ backend/models/clothingItem.js
@@ -5,7 +5,12 @@ const { ObjectId } = mongoose.Schema.Types;
 
 const clothingItemSchema = new mongoose.Schema({
   name: { type: String, minLength: 2, maxLength: 30, required: true },
-  weather: { type: String, required: true, enum: ["hot", "cold", "warm"] },
+  weather: {
+    type: String,
+    required: true,
+    enum: ["hot", "cold", "warm"],
+    lowercase: true,
+  },
   imageUrl: {
     type: String,
     validate: {
